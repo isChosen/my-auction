19dd5ef82c28bf2c1a547a15b8fe669ed3073ab0
isChosen <isChosen@163.com>
2018-05-07 00:42:33 +0800

    auction-going to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
+import { StarsComponent } from './stars/stars.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    FooterComponent,
+    ProductComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
